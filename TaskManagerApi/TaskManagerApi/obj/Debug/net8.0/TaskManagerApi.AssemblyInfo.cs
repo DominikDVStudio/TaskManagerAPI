65f7841f6b5e35bb44ea70728c874f981b0f5fc8
DearVictoryStudio <dominik8.miaskowski@gmail.com>
2026-03-13 10:08:45 +0100

    Rebuild project

--- TaskManagerApi/TaskManagerApi/obj/Debug/net8.0/TaskManagerApi.AssemblyInfo.cs
+++ TaskManagerApi/TaskManagerApi/obj/Debug/net8.0/TaskManagerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad57fc21db569085fc14aba9b38997fb8541358d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbf6d9cc54fc98e9661ff2d1e1c64579a34c86ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
